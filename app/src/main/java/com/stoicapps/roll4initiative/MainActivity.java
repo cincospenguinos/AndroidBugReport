08f318bf65717343ea8de944ed52c1937e6ec9e1
Andre LaFleur <andre.lafleur@utah.edu>
2020-04-07 09:03:43 -0600

    Initial commit
    - I am starting all over again, again

---
+++ app/src/main/java/com/stoicapps/roll4initiative/MainActivity.java
@@ -0,0 +1,14 @@
+package com.stoicapps.roll4initiative;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
