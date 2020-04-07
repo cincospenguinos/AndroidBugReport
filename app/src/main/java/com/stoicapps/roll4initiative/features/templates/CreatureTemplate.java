acc0aec0963e2f657236394c90150b0c2a0a3c2d
Andre LaFleur <andre.lafleur@utah.edu>
2020-04-07 10:00:08 -0600

    Add dice to CreatureTemplate

--- app/src/main/java/com/stoicapps/roll4initiative/features/templates/CreatureTemplate.java
+++ app/src/main/java/com/stoicapps/roll4initiative/features/templates/CreatureTemplate.java
@@ -10,7 +10,6 @@ import org.apache.commons.lang3.builder.HashCodeBuilder;
 import io.objectbox.annotation.Convert;
 import io.objectbox.annotation.Entity;
 import io.objectbox.annotation.Id;
-import io.objectbox.annotation.Transient;
 
 @Entity
 public class CreatureTemplate {
@@ -23,7 +22,7 @@ public class CreatureTemplate {
     @Convert(converter = RandomizedValueConverter.class, dbType = String.class)
     private RandomizedValue initiative;
 
-    @Transient
+    @Convert(converter = RandomizedValueConverter.class, dbType = String.class)
     private RandomizedValue hitPoints;
 
     public CreatureTemplate() {}
@@ -80,5 +79,13 @@ public class CreatureTemplate {
 
     public void setInitiative(RandomizedValue initiative) {
         this.initiative = initiative;
+    }
+
+    public RandomizedValue getHitPoints() {
+        return hitPoints;
+    }
+
+    public void setHitPoints(RandomizedValue hitPoints) {
+        this.hitPoints = hitPoints;
     }
 }
--- app/src/test/java/com/stoicapps/roll4initiative/features/templates/CreatureTemplateTest.java
+++ app/src/test/java/com/stoicapps/roll4initiative/features/templates/CreatureTemplateTest.java
@@ -2,13 +2,34 @@ package com.stoicapps.roll4initiative.features.templates;
 
 import com.stoicapps.roll4initiative.shared.RandomizedValue;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.File;
+
+import io.objectbox.Box;
+import io.objectbox.BoxStore;
+import io.objectbox.DebugFlags;
 
 import static org.junit.Assert.*;
 
 public class CreatureTemplateTest {
+
+    private static final File TEST_DIRECTORY = new File("test-db");
+    private BoxStore boxStore;
+
+    @Before
+    public void setup() {
+        BoxStore.deleteAllFiles(TEST_DIRECTORY);
+
+        boxStore = MyObjectBox.builder()
+                .directory(TEST_DIRECTORY)
+                .debugFlags(DebugFlags.LOG_QUERIES | DebugFlags.LOG_QUERY_PARAMETERS)
+                .build();
+    }
+
     @Test
-    public void creatureTemplate_equalsChecksIds() {
+    public void equals_ChecksIds() {
         CreatureTemplate template = new CreatureTemplate();
         template.setId(1);
 
@@ -19,12 +40,26 @@ public class CreatureTemplateTest {
     }
 
     @Test
-    public void creatureTemplate_noIdsChecksOtherValues() {
+    public void equals_noIdsChecksOtherValues() {
         CreatureTemplate template = new CreatureTemplate("Joe", RandomizedValue.fromString("1d20"),
                 RandomizedValue.fromString("10"));
         CreatureTemplate other = new CreatureTemplate("Joe", RandomizedValue.fromString("1d20"),
                 RandomizedValue.fromString("10"));
 
         assertTrue(template.equals(other));
+    }
+
+    @Test
+    public void crudWorks() {
+        Box<CreatureTemplate> box = boxStore.boxFor(CreatureTemplate.class);
+        CreatureTemplate template = new CreatureTemplate("Joe", RandomizedValue.fromString("1d20"),
+                RandomizedValue.fromString("10"));
+
+        assertEquals(0, box.count());
+        box.put(template);
+        assertEquals(1, box.count());
+
+        CreatureTemplate other = box.get(template.getId());
+        assertTrue(template.hashCode() == other.hashCode());
     }
 }
